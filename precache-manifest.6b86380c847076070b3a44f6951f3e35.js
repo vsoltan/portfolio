ed3e14b9d3cba74b8b1ce220305524d7316b8bc2
Valeriy Soltan <vsoltan@umass.edu>
2020-07-31 20:12:18 -0400

    Updates

---
+++ precache-manifest.6b86380c847076070b3a44f6951f3e35.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d21c2ee22e687a6aa7d93dd569e051",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0b35dccbf37531c149e2",
+    "url": "/portfolio/static/css/main.1e6015f2.chunk.css"
+  },
+  {
+    "revision": "1555b37addfd6ec9987e",
+    "url": "/portfolio/static/js/2.1718ae17.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/portfolio/static/js/2.1718ae17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b35dccbf37531c149e2",
+    "url": "/portfolio/static/js/main.932d288a.chunk.js"
+  },
+  {
+    "revision": "4630d88b084dbda85285",
+    "url": "/portfolio/static/js/runtime-main.d7e2f213.js"
+  },
+  {
+    "revision": "1697b9c56219a2565af5b13f90edcffb",
+    "url": "/portfolio/static/media/ball-array.1697b9c5.png"
+  },
+  {
+    "revision": "4979bd21b16f5acb416429b18beb421c",
+    "url": "/portfolio/static/media/ball-shadow.4979bd21.png"
+  },
+  {
+    "revision": "32c40d6fa2f96bac6be16b6d85329a60",
+    "url": "/portfolio/static/media/board.32c40d6f.png"
+  },
+  {
+    "revision": "1a103d6d4ac5fee828119b186ed86b82",
+    "url": "/portfolio/static/media/cow-acceled.1a103d6d.png"
+  },
+  {
+    "revision": "34f28a706bb89374e8040ecb0659f7f2",
+    "url": "/portfolio/static/media/metropolis-bold.34f28a70.woff"
+  },
+  {
+    "revision": "58520fd0adb68488da29ab1d50f80889",
+    "url": "/portfolio/static/media/metropolis-light.58520fd0.woff"
+  },
+  {
+    "revision": "f99a2208c1de723f5c8f0c5634df7720",
+    "url": "/portfolio/static/media/metropolis-regular.f99a2208.woff"
+  },
+  {
+    "revision": "8ae149da4c407c0419ce7ef64c89b2f6",
+    "url": "/portfolio/static/media/metropolis-thin.8ae149da.woff"
+  },
+  {
+    "revision": "8b4d139a8ac3614b34c429efd694eb0e",
+    "url": "/portfolio/static/media/ops-tile.8b4d139a.png"
+  },
+  {
+    "revision": "a2772c68855b6d5051aba4185b7dd2de",
+    "url": "/portfolio/static/media/prof.a2772c68.jpeg"
+  },
+  {
+    "revision": "143f07e9f51a72ee51fb62d35f6f3247",
+    "url": "/portfolio/static/media/rtx-tile.143f07e9.png"
+  },
+  {
+    "revision": "dbe662fd86884b8611a1be1c8ec5c5ac",
+    "url": "/portfolio/static/media/slide-config.dbe662fd.png"
+  },
+  {
+    "revision": "8f983faf4a9865530a2247cfd00f579c",
+    "url": "/portfolio/static/media/slide-qr.8f983faf.png"
+  },
+  {
+    "revision": "8d50257854a11b4229f8644c35ef32fb",
+    "url": "/portfolio/static/media/slide-tile.8d502578.png"
+  },
+  {
+    "revision": "fd3b746481bf1fc914bd960768417a5e",
+    "url": "/portfolio/static/media/teapot.fd3b7464.png"
+  },
+  {
+    "revision": "d108d1883aa0bca3fb287c9e661a0443",
+    "url": "/portfolio/static/media/watch-app.d108d188.jpeg"
+  },
+  {
+    "revision": "34999225e2e09b142de7c51d303da218",
+    "url": "/portfolio/static/media/watch-music.34999225.jpeg"
+  },
+  {
+    "revision": "52e48d32fac50b2f5d83c6f68d5fe785",
+    "url": "/portfolio/static/media/watch-timer.52e48d32.jpeg"
+  },
+  {
+    "revision": "50dec4d4339662b2176e836edc43fbc3",
+    "url": "/portfolio/static/media/watch-wear.50dec4d4.jpeg"
+  }
+]);
